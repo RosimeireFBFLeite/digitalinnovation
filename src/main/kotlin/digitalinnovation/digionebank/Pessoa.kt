8575e92518d0112a050ccde9dd9d4685e2149fff
Rosimeire F B F Leite <rosimeireflora@gmail.com>
2021-07-20 15:08:08 -0300

    primeiro commit

---
+++ src/main/kotlin/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,15 @@
+package digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Rosimeire"
+    var cpf: String = "123.123.123-12"
+
+}
+fun main(){
+    val rosimeire = Pessoa()
+
+    println(rosimeire.nome)
+    println(rosimeire.cpf)
+
+
+}
