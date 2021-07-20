683c42c0cf5b9c802f5277d4371b34747a95a68e
Rosimeire F B F Leite <rosimeireflora@gmail.com>
2021-07-20 15:18:13 -0300

    update to appty the private to set function of cpf property

--- src/main/kotlin/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/digitalinnovation/digionebank/Pessoa.kt
@@ -3,13 +3,19 @@ package digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Rosimeire"
     var cpf: String = "123.123.123-12"
+    private set
 
+    inner class Endereco {
+        var rua: String = "Rua Do Lara"
+   }
 }
 fun main(){
     val rosimeire = Pessoa()
 
+
+    println(rosimeire)
     println(rosimeire.nome)
     println(rosimeire.cpf)
 
-
+    println(rosimeire.Endereco().rua)
 }
